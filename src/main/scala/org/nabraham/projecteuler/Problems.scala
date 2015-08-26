20179cde4803d1d35fde45a9d7b2af527da2b8ee
Nathan Abraham <nabraham@gmail.com>
2015-08-26 16:22:39 -0400

    49....yuck

--- src/main/scala/org/nabraham/projecteuler/Problems.scala
+++ src/main/scala/org/nabraham/projecteuler/Problems.scala
@@ -852,4 +852,25 @@ object Problems {
     assert((1 to 1000).map(x => BigInt(x).pow(x)).sum.toString.takeRight(10) == solutions("48"))
   }
   problemMap += ("048" -> p048)
+
+  //  The arithmetic sequence, 1487, 4817, 8147, in which each of the terms increases by 3330, is unusual in two ways:
+  //  (i) each of the three terms are prime, and,
+  //  (ii) each of the 4-digit numbers are permutations of one another.
+  //
+  //   There are no arithmetic sequences made up of three 1-, 2-, or 3-digit primes, exhibiting this property, but
+  //   there is one other 4-digit increasing sequence.
+  //
+  //   What 12-digit number do you form by concatenating the three terms in this sequence?
+  def p049(): Unit = {
+    val candidates = primes.dropWhile(_ < 1000).takeWhile(_ < 10000).groupBy(_.toString.sorted).filter(_._2.size >= 3)
+    def abs(n: Int): Int = { if (n > 0) n else -n }
+    def commonDiff(nums: List[Int]): Boolean = {
+      nums.sorted.sliding(2).map(w => w(1) - w(0)).toList.distinct.size == 1
+    }
+    val rez = candidates.map(_._2.toList).map(_.combinations(3).filter(commonDiff).toList).filter(_.nonEmpty).toList
+    val other = rez.filter(_.flatten.head != 1487).flatten.flatten
+    assert(other.mkString("") == solutions("49"))
+  }
+  problemMap += ("049" -> p049)
+
 }
